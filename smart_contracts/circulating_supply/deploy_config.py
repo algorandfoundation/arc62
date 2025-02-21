64e740c4c363cfadfe5b46ceef1fdccfb993ae2e
cusma <cosimo.bassi@algorand.foundation>
2025-02-21 16:18:04 -0300

    chore: autopopulate resources, add asset fields
    Signed-off-by: cusma <cosimo.bassi@algorand.foundation>

--- smart_contracts/circulating_supply/deploy_config.py
+++ smart_contracts/circulating_supply/deploy_config.py
@@ -2,9 +2,13 @@ import logging
 from typing import Final
 
 import algokit_utils
+from algokit_utils.config import config
 
 logger = logging.getLogger(__name__)
 
+ASA_NAME: Final[str] = "ARC-62 Test ASA"
+ASA_UNIT_NAME: Final[str] = "ARC-62"
+ASA_DECIMALS: Final[int] = 0
 ASA_TOTAL: Final[int] = 42
 APP_URI: Final[str] = "algorand://app/"
 
@@ -14,6 +18,11 @@ def deploy() -> None:
     from smart_contracts.artifacts.circulating_supply.circulating_supply_client import (
         CirculatingSupplyFactory,
         SetAssetArgs,
+    )
+
+    config.configure(
+        debug=False,
+        populate_app_call_resources=True,
     )
 
     algorand = algokit_utils.AlgorandClient.from_environment()
@@ -32,7 +41,10 @@ def deploy() -> None:
         algokit_utils.AssetCreateParams(
             sender=deployer.address,
             signer=deployer.signer,
+            asset_name=ASA_NAME,
+            unit_name=ASA_UNIT_NAME,
             total=ASA_TOTAL,
+            decimals=ASA_DECIMALS,
             manager=deployer.address,
             url=APP_URI + str(app_client.app_id),
         )
