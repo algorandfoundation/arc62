2fefe8009a51313c82080d1daba6a6faf5eb7970
cusma <cosimo.bassi@algorand.foundation>
2025-05-21 13:18:39 +0200

    chore: restore labels
    Signed-off-by: cusma <cosimo.bassi@algorand.foundation>

--- smart_contracts/circulating_supply/config.py
+++ smart_contracts/circulating_supply/config.py
@@ -3,4 +3,4 @@ from typing import Final
 # Roles
 NOT_CIRCULATING_LABEL_1: Final[str] = "burned"
 NOT_CIRCULATING_LABEL_2: Final[str] = "locked"
-NOT_CIRCULATING_LABEL_3: Final[str] = "test"
+NOT_CIRCULATING_LABEL_3: Final[str] = "generic"
